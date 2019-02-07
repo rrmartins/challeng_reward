3e77c7145210bec42033cd47114ddd3b254750c4
Rodrigo Martins <rrmartinsjg@gmail.com>
2019-02-06 23:49:23 -0200

    new controller

--- app/controllers/invites/calculates_controller.rb
+++ app/controllers/invites/calculates_controller.rb
@@ -1,6 +1,21 @@
 module Invites
   class CalculatesController < ApplicationController
     def update
+      if calculate_call
+        render status: :ok, json: @calculate_call.calc_result
+      else
+        render status: :unprocessable_entity, json: { errors: @calculate_call.errors }
+      end
+    end
+
+    private
+
+    def calculate_call
+      @calculate_call ||= InvitesService::Calculate.call(file)
+    end
+
+    def file
+      params[:file]
     end
   end
 end
