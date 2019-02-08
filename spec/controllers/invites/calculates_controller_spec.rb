d2971cd93b9acff13fcb643f4047928c12588da7
Rodrigo Martins <rrmartinsjg@gmail.com>
2019-02-08 02:24:04 -0200

    spec finished calculates controller

--- spec/controllers/invites/calculates_controller_spec.rb
+++ spec/controllers/invites/calculates_controller_spec.rb
@@ -2,33 +2,28 @@ require 'rails_helper'
 
 RSpec.describe Invites::CalculatesController, type: :controller do
   describe '#update' do
-
-    let(:file) { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/myfiles/invites_ok.txt')) }
-
-    let(:params) { { file: file } }
-
-    subject(:post_update) { patch :update, params: params, xhr: true }
+    subject(:post_update) { put :update, xhr: true }
 
     before do
-      allow(InvitesService::Calculate).to receive(:call).with(file) { return_service }
+      allow(InvitesService::Calculate).to receive(:call) { return_service }
       post_update
     end
 
     describe 'JSON' do
       context 'when success' do
-        let(:calc_result) { { 'A': 1.75, 'B': 1.5, 'C': 1 } } 
-        let(:return_service) { OpenStruct.new(calc_result: calc_result, errors: {} ) } 
+        let(:calc_result) { { 'A': 1.75, 'B': 1.5, 'C': 1 } }
+        let(:return_service) { OpenStruct.new(calculated: calc_result, errors: nil ) }
 
         it { expect(response).to have_http_status :ok }
-        it { expect(InvitesService::Calculate).to have_received(:call).with(file) }
+        it { expect(InvitesService::Calculate).to have_received(:call) }
       end
 
       context 'when not success' do
         let(:calc_result) { { } }
-        let(:return_service) { OpenStruct.new(calc_result: calc_result, errors: { extension_error: 'extension_error' } ) } 
+        let(:return_service) { OpenStruct.new(calculated: calc_result, errors: { extension_error: 'extension_error' } ) }
 
         it { expect(response).to have_http_status :unprocessable_entity }
-        it { expect(InvitesService::Calculate).to have_received(:call).with(file) }
+        it { expect(InvitesService::Calculate).to have_received(:call) }
       end
     end
   end
